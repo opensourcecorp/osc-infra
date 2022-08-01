9f3579ecf30bf16dabbcb8aa2edff04de83ef383
Ryan J. Price <ryapric@gmail.com>
2022-08-01 09:39:56 -0500

    Update TF HTTP provider refs since .body is deprecated

--- infracode/providers/aws/ec2_instance/outputs.tf
+++ infracode/providers/aws/ec2_instance/outputs.tf
@@ -1,4 +1,4 @@
 output "my_ip" {
   description = "IP address of the deployer host. Can be used to e.g. scope caller Security Groups"
-  value       = "${chomp(data.http.my_ip.body)}/32"
+  value       = "${chomp(data.http.my_ip.response_body)}/32"
 }
--- infracode/providers/aws/ec2_instance/security_group.tf
+++ infracode/providers/aws/ec2_instance/security_group.tf
@@ -26,7 +26,7 @@ resource "aws_security_group_rule" "ping" {
   from_port         = 8
   to_port           = 0
   protocol          = "icmp"
-  cidr_blocks       = ["${chomp(data.http.my_ip.body)}/32"]
+  cidr_blocks       = ["${chomp(data.http.my_ip.response_body)}/32"]
   security_group_id = aws_security_group.main.id
 }
 
@@ -36,7 +36,7 @@ resource "aws_security_group_rule" "deployer_ssh" {
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  cidr_blocks       = ["${chomp(data.http.my_ip.body)}/32"]
+  cidr_blocks       = ["${chomp(data.http.my_ip.response_body)}/32"]
   security_group_id = aws_security_group.main.id
 }
 
